9021c361b2c2b57efdb5d9be50511e4bd6175b6d
pk-218 <2182001pk@gmail.com>
2021-07-04 13:59:22 +0530

    Improved navbar to prevent additional requests

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-    return ( 
-        <Navbar bg="dark" variant="dark" className="mb-3">
-            <Navbar.Brand href="#home">ImageMart</Navbar.Brand>
-            <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/list">Images</Nav.Link>
-            </Nav>
-      </Navbar>
-     );
-}
- 
+  return (
+    <Navbar bg="dark" variant="dark" className="mb-3">
+      <Navbar.Brand href="#home">ImageMart</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link as={Link} to="/">
+          Home
+        </Nav.Link>
+        <Nav.Link as={Link} to="/list">
+          Images
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+};
+
 export default Navigation;
